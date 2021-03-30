4e43052f1f63f622e5ba8be03406efd6b647128d
Cesare Bek <cesarebek1@gmail.com>
2021-03-30 14:06:16 +0200

    scripts

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMusic } from '@fortawesome/free-solid-svg-icons';
-import { library } from '@fortawesome/fontawesome-svg-core';
+
 const Nav = ({ libraryStatus, setLibraryStatus }) => {
   return (
     <nav>
